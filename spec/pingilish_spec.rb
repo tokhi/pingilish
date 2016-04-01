0965301d8d46f8696646446aaf80d6b29e435cb4
tokhi <shafi.tokhi@gmail.com>
2016-04-01 17:21:38 +0200

    spec tests

--- spec/pingilish_spec.rb
+++ spec/pingilish_spec.rb
@@ -1,11 +1,30 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe Pingilish do
+
   it 'has a version number' do
     expect(Pingilish::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#to_default'	do
+    let(:input) {'نیکی چو از حد بگذرد نادان خیال بد کند.'}
+    let(:output) {subject.to_default(input)}
+
+
+    it 'convert farsi to pinglish' do
+      expect(output).to eq("ny ky  cho az hd bgzrd nadan khy al bd knd.")
+
+    end
+  end
+
+  describe '#to_farsi'	do
+    let(:input) {"nabordeh ranj ganj moiasar namishawad"}
+    let(:output) {subject.to_farsi(input)}
+
+    it 'convert piniglish to farsi' do
+      expect(output).to match("نبرده رنج گنج میسر نمیشود ")
+
+    end
   end
 end
